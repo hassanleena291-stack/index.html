1bca3bb347409dddbd06e13496942fe4b1569197
lobnahassan <hassanleena291@gmail.com>
2025-12-17 04:51:59 -1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+//search
+
+let search = document.querySelector('.search-box');
+document.querySelector('#search-icon').onclick = () => {
+    search.classList.toggle('active');
+    navbar.classList.remove('active');
+}
+//navbar
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-icon').onclick = () => {
+    navbar.classList.toggle('active');
+    search.classList.remove('active');
+
+}
+//remove menue and search icon
+
+window.onscroll = () => {
+    search.classList.toggle('active');
+    navbar.classList.remove('active');
+}
+
+// header animation
+
+let header = document.querySelector('header');
+window.addEventListener('scroll', () => {
+    header.classList.toggle('active', window.scrollY > 0);
+});
